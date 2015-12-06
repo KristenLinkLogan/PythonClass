bd52d59f8d85bbd1955fc48833a81ff646174e5d
Kristen Link Logan <KristenLinkLogan@users.noreply.github.com>
2015-12-05 22:02:51 -0500

    Create Playtime2_states.py

---
+++ Playtime2_states.py
@@ -0,0 +1,12 @@
+#program to print an html drop down menu for all 50 states
+
+#define list of states
+states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District Of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+abbreviations = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+
+print "<select name='State'>"
+
+for abbr,state in zip(abbreviations,states):
+	print '\t<option value="{0}">{1}</option>'.format(abbr,state)
+
+print "</select>"
